79fad3ac4371cf0b0e31498d6aeead479ffc1c1a
ilmerkul <merkuloviv@my.msu.ru>
2025-04-24 23:57:18 +0300

    add launch

--- ros_robotics/src/control/setup.py
+++ ros_robotics/src/control/setup.py
@@ -1,27 +1,30 @@
+import glob
+import os
+
 from setuptools import find_packages, setup
 
-package_name = 'control'
+package_name = "control"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob.glob("launch/*")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='merkul',
-    maintainer_email='merkuloviv@my.msu.ru',
-    description='Control robot package',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="merkul",
+    maintainer_email="merkuloviv@my.msu.ru",
+    description="Control robot package",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'observation_publisher = control.observation_publisher:main',
-            'planner_subsriber = control.planner_subsriber:main',
+        "console_scripts": [
+            "observation_publisher = control.observation_publisher:main",
+            "planner_subsriber = control.planner_subsriber:main",
         ],
     },
 )
