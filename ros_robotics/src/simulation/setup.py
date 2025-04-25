68e191e0d18461293ca0e3bc546bec8b45d0fe00
root <merkuloviv@my.msu.ru>
2025-04-25 19:10:35 +0300

    add sim package

---
+++ ros_robotics/src/simulation/setup.py
@@ -0,0 +1,27 @@
+import glob
+import os
+
+from setuptools import find_packages, setup
+
+package_name = "simulation"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob.glob("launch/*")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="merkul",
+    maintainer_email="merkuloviv@my.msu.ru",
+    description="Package for simulation",
+    license="MIT",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": ["sim = simulation.sim:main"],
+    },
+)
