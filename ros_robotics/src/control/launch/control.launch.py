9530a1bb8815221a90bd8eb68874d442ee0e5e17
root <merkuloviv@my.msu.ru>
2025-05-02 13:20:06 +0300

    set build and launch control package

--- ros_robotics/src/control/launch/control.launch.py
+++ ros_robotics/src/control/launch/control.launch.py
@@ -1,25 +1,127 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+package_name = "control"
+package_name_description = "description"
 
 
 def generate_launch_description():
-    ld = LaunchDescription()
-
-    observation_publisher_node = Node(
-        package="control",
-        executable="observation_publisher",
-        name="observation_publisher",
-        namespace="observation",
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time", description="Use sim (gazebo) time", default_value="true"
+    )
+    control_file_arg = DeclareLaunchArgument(
+        name="control_file",
+        default_value="robot.yaml",
+        description="Control file .yaml",
+    )
+    xacro_file_arg = DeclareLaunchArgument(
+        name="xacro_file",
+        description="Name of xacro file (if use_urdf=false)",
     )
 
-    planner_subscriber_node = Node(
-        package="control",
-        executable="planner_subscriber",
-        name="planner_subscriber",
-        namespace="planner",
+    launch_args = [use_sim_time_arg, control_file_arg, xacro_file_arg]
+    control_file = PathJoinSubstitution(
+        [
+            FindPackageShare(package_name),
+            "config",
+            LaunchConfiguration("control_file"),
+        ]
+    )
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    xacro_file = PathJoinSubstitution(
+        [
+            FindPackageShare(package_name_description),
+            "urdf",
+            LaunchConfiguration("xacro_file"),
+        ]
     )
 
-    ld.add_action(observation_publisher_node)
-    ld.add_action(planner_subscriber_node)
+    robot_description_content = Command(
+        ["xacro ", xacro_file, " use_sim_time:=", use_sim_time]
+    )
 
-    return ld
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description_content, control_file],
+        output="both",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+            "--switch-timeout",
+            "10",
+        ],
+    )
+
+    robot_position_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "position_controller",
+            "--controller-manager",
+            "/controller_manager",
+            "--param-file",
+            control_file,
+            "--switch-timeout",
+            "10",
+        ],
+    )
+
+    control_node = Node(
+        package=package_name,
+        executable="control_node",
+        name="control_node",
+        namespace="control_node",
+    )
+
+    # robot_controller_gazebo = Node(
+    #    package="bruno_controller",
+    #    executable="robot_controller_gazebo",
+    #    name="robot_controller_gazebo",
+    #    output="screen",
+    #    parameters=[],
+    # )
+
+    # joypad_node = Node(
+    #    package="joypad",
+    #    executable="ramped_joypad.py",
+    #    name="ramped_joypad",
+    #    output="screen",
+    #    parameters=[],
+    # )
+
+    delay_robot_postion_controller_spawner_after_joint_state_broadcaster_spawner = (
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[robot_position_controller_spawner],
+            )
+        )
+    )
+
+    delay_control_node_after_robot_position_controller_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=robot_position_controller_spawner,
+            on_exit=[control_node],
+        )
+    )
+
+    return LaunchDescription(
+        [
+            *launch_args,
+            ros2_control_node,
+            joint_state_broadcaster_spawner,
+            delay_robot_postion_controller_spawner_after_joint_state_broadcaster_spawner,
+            delay_control_node_after_robot_position_controller_spawner,
+        ]
+    )
--- ros_robotics/src/control/setup.py
+++ ros_robotics/src/control/setup.py
@@ -13,6 +13,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         (os.path.join("share", package_name, "launch"), glob.glob("launch/*")),
+        (os.path.join("share", package_name, "config"), glob.glob("config/*")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -25,6 +26,7 @@ setup(
         "console_scripts": [
             "observation_publisher = control.observation_publisher:main",
             "planner_subscriber = control.planner_subscriber:main",
+            "control_node = control.control_node:main",
         ],
     },
 )
